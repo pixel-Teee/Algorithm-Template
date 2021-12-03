902670bb08630f3e4fd364d3179cc755b54155ae
pixel-Teee <1173769234@qq.com>
2021-12-03 17:14:09 +0800

    add quick sort

---
+++ 基础算法/快速排序(应用)(第k个数).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+const int N = 100005;
+int q[N];
+
+int quick_sort(int l, int r, int k)
+{
+    if(l >= r) return q[l];
+    int i = l - 1, j = r + 1, x = q[l + r >> 1];
+    while(i < j)
+    {
+        do i++; while(q[i] < x);
+        do j--; while(q[j] > x);
+        if(i < j) swap(q[i], q[j]);
+    }
+    int sl = j - l + 1;
+    if(k <= sl) return quick_sort(l, j, k);
+    else return quick_sort(j + 1, r, k - sl);
+}
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i) cin >> q[i];
+    
+    cout << quick_sort(1, n, k) << endl;
+    return 0;
+}
